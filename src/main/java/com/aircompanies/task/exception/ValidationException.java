a71c5b2b0d20e10c660d0a40edad9ac41c0ac144
Taras Shapran <shaprantaras2000@gmail.com>
2021-05-20 20:55:39 +0300

    add ValidationException

---
+++ src/main/java/com/aircompanies/task/exception/ValidationException.java
@@ -0,0 +1,10 @@
+package com.aircompanies.task.exception;
+
+public class ValidationException extends RuntimeException{
+    public ValidationException() {
+    }
+
+    public ValidationException(String message) {
+        super(message);
+    }
+}
